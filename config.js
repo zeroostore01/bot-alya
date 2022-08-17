2f6b7dd72194f82b31d0a8f8d83f82d599bfa5cf
Alya <96687229+alya-tok@users.noreply.github.com>
2022-08-17 14:05:35 +0700

    update config.js

--- config.js
+++ config.js
@@ -30,6 +30,7 @@ global.limitawal = {
     premium: "Infinity",
     free: 100
 }
+global.gambar = 'https://telegra.ph/file/7858fc96fffda74b3ab44.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
 global.sc = fs.readFileSync('./media/sc.jpg')
 global.tq = fs.readFileSync('./media/tq.jpg')
 global.menu = fs.readFileSync('./media/menu.jpg')
