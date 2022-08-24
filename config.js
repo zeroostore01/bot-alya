5aa1f040e67d593036ee63a85675e929128b519c
zeroostore01 <107806866+zeroostore01@users.noreply.github.com>
2022-08-25 04:49:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // Other
-global.owner = ['6285815975368']
-global.premium = ['6285815975368']
-global.ownername = 'Liya'
-global.botname = 'AliyaBot'
-global.packname = '© by AliyaBot'
-global.gc = 'https://bit.ly/3KMlmv2'
+global.owner = ['6283890585717']
+global.premium = ['6283890585717']
+global.ownername = '𝒁 𝑬 𝑹 𝑶'
+global.botname = '𝒁 𝑬 𝑹 𝑶-𝑴𝑫'
+global.packname = '© by Zeroo'
+global.gc = 'https://tinyurl.com/2el4odke'
 global.linkyt = 'https://youtube.com/channel/UC9Si3U0o9dGS9MDfJR5iF6Q'
-global.linkgc = 'https://chat.whatsapp.com/DwP6uHYBWBc6TBSsNJrzwN'
+global.linkgc = 'https://chat.whatsapp.com/E8IwFUibPjkDgilRKw6JBg'
 global.limitawal = '100'
-global.author = '@alya.xzy'
+global.author = '@Zeroo Store'
 global.sessionName = 'polo'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = 'terserah'
@@ -30,12 +30,12 @@ global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-global.gambar = 'https://telegra.ph/file/7858fc96fffda74b3ab44.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
+global.gambar = 'https://telegra.ph/file/ac158e968d8856be8fcc6.jpg' //disini gambar bot nya lu bisa ganti pink gambar ini dengan link gambar punya lu
 global.sc = fs.readFileSync('./media/sc.jpg')
 global.tq = fs.readFileSync('./media/tq.jpg')
 global.menu = fs.readFileSync('./media/menu.jpg')
 global.allmenu = fs.readFileSync('./media/allmenu.jpg')
-global.thumb = { url: 'https://i.pinimg.com/736x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg' }
+global.thumb = { url: 'https://telegra.ph/file/e30e241f1da315e5a486f.jpg' }
 global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
 
 let file = require.resolve(__filename)
